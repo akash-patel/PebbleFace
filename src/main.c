619fc7a0aca0b3d50d25c826e635cd0f2dc9fab0
Akash Patel <akash-patel@users.noreply.github.com>
2014-10-12 00:43:20 -0400

    finished

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 
 Window *g_window;
 TextLayer *g_text_layer;
+TextLayer *g_Akash;
+TextLayer *g_Patel;
 GBitmap *g_frame_bitmap;
 BitmapLayer *g_frame_layer;
 
@@ -10,24 +12,45 @@ void window_load(Window *window){
     
     //Create and add the image
     
-    g_frame_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FRAME);
+    /*g_frame_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FRAME);
     g_frame_layer = bitmap_layer_create(GRect(0, 0, 129, 60));
     bitmap_layer_set_bitmap(g_frame_layer, g_frame_bitmap);
     bitmap_layer_set_alignment(g_frame_layer, GAlignRight);
-    layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(g_frame_layer));
+    layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(g_frame_layer));*/
     
+    g_Akash = text_layer_create(GRect(0,10,144,56));
+    text_layer_set_background_color(g_Akash, GColorClear);
+    text_layer_set_text_color(g_Akash, GColorBlack);
     
+    text_layer_set_font(g_Akash,fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
+    text_layer_set_text_alignment(g_Akash, GTextAlignmentCenter);
+    
+    text_layer_set_text(g_Akash, "Akash");
+    
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(g_Akash));
     
     //We will add the creation of the window's elements here soon!
-    g_text_layer = text_layer_create(GRect(0,59,144,50)); //0,59 <- top left corner, 144 is the width, 50 is the height
-    text_layer_set_background_color(g_text_layer,GColorClear);
-    text_layer_set_text_color(g_text_layer, GColorBlack);
+    g_text_layer = text_layer_create(GRect(0,56,144,56)); //0,59 <- top left corner, 144 is the width, 50 is the height
+    text_layer_set_background_color(g_text_layer,GColorBlack);
+    text_layer_set_text_color(g_text_layer, GColorWhite);
     
     //improve the layour to be more like a watchface
     text_layer_set_font(g_text_layer,fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS));
     text_layer_set_text_alignment(g_text_layer, GTextAlignmentCenter);
     
     layer_add_child(window_get_root_layer(window), text_layer_get_layer(g_text_layer));
+    
+    
+    g_Patel = text_layer_create(GRect(0,117,144,56));
+    text_layer_set_background_color(g_Patel, GColorClear);
+    text_layer_set_text_color(g_Patel, GColorBlack);
+    
+    text_layer_set_font(g_Patel,fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
+    text_layer_set_text_alignment(g_Patel, GTextAlignmentCenter);
+    
+    text_layer_set_text(g_Patel, "Patel");
+    
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(g_Patel));
     
     //text_layer_set_text(g_text_layer, "Doggy doggg");
     
@@ -38,6 +61,8 @@ void window_unload(Window *window){
     //we wills afely destroy the window's elements here!
     
     text_layer_destroy(g_text_layer);
+    text_layer_destroy(g_Akash);
+    text_layer_destroy(g_Patel);
     
     gbitmap_destroy(g_frame_bitmap);
     bitmap_layer_destroy(g_frame_layer);
